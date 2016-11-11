9c3d348e717267bd10086969719e01bccb89e98e
Nickolay <nickolay02@inbox.ru>
2016-11-11 23:11:46 +0200

    Added 3 tests and fixed some bugs

--- lib/htmlutils/macro.rb
+++ lib/htmlutils/macro.rb
@@ -1,7 +1,6 @@
 module HtmlUtils
-  puts self.instance_methods
-
-  def loadScripts(urls, type="text/javascript")
+  # Generates script-loading code
+  def self.loadScripts(urls, type="text/javascript")
     str = ""
     urls.each do |url|
       str << %{<script type="#{type}" src="#{url}"></script>\n}
@@ -9,27 +8,30 @@ module HtmlUtils
     str
   end
   
-  def loadStyles(urls, type="text/css")
+  # Generates style-loading code
+  def self.loadStyles(urls, type="text/css")
     str = ""
     urls.each do |url|
-      str << %{<link type="#{type}" rel="stylesheet" src="#{url}"}
+      str << %{<link type="#{type}" rel="stylesheet" href="#{url}">\n}
     end
+    str
   end
   
-  def self.makeVTable(data)
+  # Generates vertical table
+  def self.makeVTable(data, mainAttr="", trhAttr="", thAttr="", trAttr="", tdAttr="")
     str = ""
-    str << "<table>\n"
-    str << " <tr>\n"
+    str << "<table #{mainAttr}>\n"
+    str << " <tr #{trhAttr}>\n"
     data.each do |d|
-      str << %{  <th>#{d[0]}</th>\n}
+      str << %{  <th #{thAttr}>#{d[0]}</th>\n}
     end
     
     str << " </tr>\n"
     
     data.each do |d|
-      str << " <tr>\n"
+      str << " <tr #{trAttr}>\n"
       d[1..-1].each do |d1|
-        str << "  <td>#{d1}</td>\n"
+        str << "  <td #{tdAttr}>#{d1}</td>\n"
       end
       str << " </tr>\n"
     end
--- test/test.rb
+++ test/test.rb
@@ -2,23 +2,39 @@ require "minitest/autorun"
 require "htmlutils"
 
 class HUTest < Minitest::Test
-  def test_table
-    assert_equal HtmlUtils.makeVTable([["Name", "go", "ruby"], ["Version", "1.7", "1.8"]]),
-<<-HTML
-<table>
- <tr>
-  <th>Name</th>
-  <th>Version</th>
+  def test_styles
+    expected = <<-HTML
+<link type="text/css" rel="stylesheet" href="/css/normalize.css">
+<link type="text/css" rel="stylesheet" href="/css/materialize.min.css">
+HTML
+    assert_equal expected, HtmlUtils.loadStyles(["/css/normalize.css", "/css/materialize.min.css"])
+  end
+
+  def test_scripts
+    expected = <<-HTML
+<script type="text/javascript" src="/js/jquery.min.js"></script>
+<script type="text/javascript" src="/js/materialize.min.js"></script>
+HTML
+    assert_equal expected, HtmlUtils.loadScripts(["/js/jquery.min.js", "/js/materialize.min.js"])
+  end
+
+  def test_vtable
+    expected = <<-HTML
+<table >
+ <tr >
+  <th >Name</th>
+  <th >Version</th>
  </tr>
- <tr>
-  <td>go</td>
-  <td>ruby</td>
+ <tr >
+  <td >go</td>
+  <td >ruby</td>
  </tr>
- <tr>
-  <td>1.7</td>
-  <td>1.8</td>
+ <tr >
+  <td >1.7</td>
+  <td >1.8</td>
  </tr>
 </table>
 HTML
+    assert_equal expected, HtmlUtils.makeVTable([["Name", "go", "ruby"], ["Version", "1.7", "1.8"]])
   end
 end
